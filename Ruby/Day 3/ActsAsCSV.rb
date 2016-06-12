d8e290b14233d8cec7cb3d964d10f372385cf4ec
Sami Kurvinen <sami.kurvinen@ray.fi>
2016-06-12 16:25:43 +0300

    Refactoring

--- Ruby/Day 3/ActsAsCSV.rb
+++ Ruby/Day 3/ActsAsCSV.rb
@@ -2,18 +2,6 @@
 # return the value for the column for a given heading
 
 module ActsAsCsv
-  # Extending and including thes are not required for the implementation, but are here for demonstration
-  def self.included(base)
-    base.extend ClassMethods
-  end
-
-  module ClassMethods
-    def acts_as_csv
-      include InstanceMethods
-    end
-  end
-
-  module InstanceMethods
     def read
       @csv_contents = []
       filename = self.class.to_s.downcase + '.txt'
@@ -21,7 +9,7 @@ module ActsAsCsv
       @headers = file.gets.chomp.split(', ')
 
       file.each do |row|
-        @csv_contents << row.chomp.split(', ')
+        @csv_contents << CsvRow.new(@headers, row.chomp.split(', '))
       end
     end
 
@@ -31,30 +19,28 @@ module ActsAsCsv
       read
     end
 
-    def each &block
-      @csv_contents.each { |row| block.call CsvRow.new(@headers, row) }
+    def each
+      @csv_contents.each { |row| yield row }
     end
-  end
-end
 
-class CsvRow
-  # Figure out how you can return CsvRow object from ActAsCsv .each
-  # Create a map of values to index
-  # Return that index in method_missing
-  attr_accessor :row
+  class CsvRow
+    # Figure out how you can return CsvRow object from ActAsCsv .each
+    # Create a map of values to index
+    # Return that index in method_missing
+    attr_accessor :row
 
-  def initialize(headers, row)
-    @row = Hash[headers.zip row]
-  end
+    def initialize(headers, row)
+      @row = Hash[headers.zip row]
+    end
 
-  def method_missing name, *args
-    @row[name.to_s]
+    def method_missing name, *args
+      @row[name.to_s]
+    end
   end
 end
 
 class RubyCsv
   include ActsAsCsv
-  acts_as_csv
 end
 
 m = RubyCsv.new
